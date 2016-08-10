37d0b4da2c7d83aa56d692f4791f32db638bb388
Matt Hosack <hosack.matt@gmail.com>
2016-08-10 10:40:45 -0700

    Making run an infinite loop that breaks on keyboard interrupt instead a finite time

--- run.py
+++ run.py
@@ -5,7 +5,11 @@ from time import sleep
 def main():
     p = Pinger(timeout=1)
     p.loop()
-    sleep(5)
+    while True:
+        try:
+            sleep(1)
+        except KeyboardInterrupt:
+            break
     p.stop()
 
 if __name__ == "__main__":
